7ed4af9a6d64676eb6db91e0f23c6f5ec97aafed
Adrian R <espinosa012@gmail.com>
2020-10-28 09:27:48 +0100

    updated

--- main.py
+++ main.py
@@ -2,16 +2,10 @@ from blogabet 	import Blogabet
 from pymongo 	import MongoClient
 from getpass 	import getpass
 
-
-
 tipster =	input('Tipster to analyze: ')
-
 
 b 	=	Blogabet()
 b.analyze_tipster(tipster)
-
-
-
 b.driver.close()
 
 
